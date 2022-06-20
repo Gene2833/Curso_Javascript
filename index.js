9e47aca8ceb79f880b906874f77d5ce216759fb0
Franklin nieto <framanic15@gmail.com>
2022-06-20 19:21:03 -0400

    Desafio complementario

---
+++ index.js
@@ -0,0 +1,17 @@
+
+//tablas de multipicar del 1 al 12
+
+for(let i =1; i<= 12; i++){
+    for(let j = 1; j<=12; j++)
+    console.log(i + "X" + j + "=" + i * j);
+}
+
+//Ejercicio 2
+
+let numero = 0;
+
+while(numero<=10){
+    console.log("Exito");
+    numero++;
+}
+
